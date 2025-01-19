e8c25b20c560e34964bb6dd34be1446825d6f84a
Thaw Myo Han <thawmyohan736@gmail.com>
2025-01-19 12:43:34 +0000

    Delete src/Final directory

--- src/Final/0administration/2Customers/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('nav');
-
-    menuToggle.addEventListener('click', function () {
-        this.classList.toggle('open');
-        nav.classList.toggle('open');
-    });
-});
